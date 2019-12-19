a35c947f208fd40de1fd068ecec1057da5506ae4
eowenDEV <57829484+eowenDEV@users.noreply.github.com>
2019-12-18 16:09:11 -0800

    Slot Machine added test styling

--- react exercises/slot machine/SlotMachine.js
+++ react exercises/slot machine/SlotMachine.js
@@ -1,19 +1,19 @@
 class SlotMachine extends React.Component {
     render() {
-        const s1 = this.props.s1,
-            s2 = this.props.s2,
-            s3 = this.props.s3;
-        let msg;
-        if(s1 === s2 && s2 === s3) {
+        const {s1, s2, s3} = this.props;
+        const winner = (s1 === s2) && (s2 === s3);
+        let msg = "";
+        console.log(winner);
+        if(winner) {
             msg = "You Win!"
         } else {
             msg = "You Lose";
         }
         return (
-            <div>
+            <div className="SlotMachine">
                 <h3>Result:</h3>
-                <p>{s1} {s2} {s3}</p>
-                <p>{msg}</p>
+                <p className="slot-results">{s1} {s2} {s3}</p>
+                <p className={winner ? "win" : "lose"}>{msg}</p>
             </div>
         )
     }
